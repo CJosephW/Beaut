86f8deb754a9e040d0183ba08b814ba230bc07c5
CJosephW <wisecameron1@gmail.com>
2021-06-10 23:56:06 -0400

    Initial

---
+++ src/JS/App.js
@@ -0,0 +1,12 @@
+import '../style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello World</h1>
+        <h2><b>Beaut</b>(JSON and JWT Beautifyer)</h2>
+    </div>
+  );
+}
+
+export default App;
